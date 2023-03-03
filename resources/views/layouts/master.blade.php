1801dbbb46779a44f2ceae4420a36ee6ec96f452
Emmanuel Makabu <emmanuelmakabu7@gmail.com>
2023-03-03 15:33:57 +0200

    ✨ Design Master layout

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <!-- title -->
+        <title>Kreative Touch :: Branding & Media Agency</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1" />
+        <meta name="author" content="ThemeZaa">
+        <!-- description -->
+        <meta name="description" content="POFO is a highly creative, modern, visually stunning and Bootstrap responsive multipurpose agency and portfolio HTML5 template with 25 ready home page demos.">
+        <!-- keywords -->
+        <meta name="keywords" content="creative, modern, clean, bootstrap responsive, html5, css3, portfolio, blog, agency, templates, multipurpose, one page, corporate, start-up, studio, branding, designer, freelancer, carousel, parallax, photography, personal, masonry, grid, coming soon, faq">
+        <!-- favicon -->
+        <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}">
+        <link rel="apple-touch-icon" href="{{ asset('assets/images/apple-touch-icon-57x57.png') }}">
+        <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('assets/images/apple-touch-icon-72x72.png') }}">
+        <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('assets/images/apple-touch-icon-114x114.png') }}">
+        <!-- style sheets and font icons  -->
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootsnav.css"') }}>
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/font-icons.min.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/theme-vendors.min.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css') }}" />
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/responsive.css ') }}" />
+    </head>
+    <body>
+        <div class="box-layout">
+            <header>
+                <!-- start navigation -->
+                <nav class="navbar navbar-default bootsnav background-white header-light navbar-scroll-top navbar-expand-lg">
+                    <div class="container-lg nav-header-container">
+                        <!-- start logo -->
+                        <div class="col-auto ps-0">
+                            <a href="index.html" title="Pofo" class="logo"><img src="{{ asset('assets/images/logo.png') }}" data-at2x="{{ asset('assets/images/logo@2x.png') }}" class="logo-dark default" alt="Pofo"><img src="{{ asset('assets/images/logo-white.png') }}" data-at2x="{{ asset('assets/images/logo-white@2x.png') }}" alt="Pofo" class="logo-light"></a>
+                        </div>
+                        <!-- end logo -->
+                        <div class="col accordion-menu pe-0 pe-md-3">
+                            <button type="button" class="navbar-toggler collapsed" data-bs-toggle="collapse" data-bs-target="#navbar-collapse-toggle-1">
+                                <span class="sr-only">toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <div class="navbar-collapse collapse justify-content-end" id="navbar-collapse-toggle-1">
+                                <ul id="accordion" class="nav navbar-nav navbar-left no-margin alt-font text-normal" data-in="animate__fadeIn" data-out="animate__fadeOut">
+                                    <li><a href="#">Accueil</a></li>
+                                    <li><a href="#">Services</a></li>
+                                    <li><a href="#">Portfolio</a></li>
+                                    <li><a href="#">Contact</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+                <!-- end navigation -->
+            </header>
+        </div>
+        <!-- start footer -->
+        <footer class="footer-modern-dark bg-extra-dark-gray padding-five-tb sm-padding-30px-tb">
+            <div class="footer-widget-area padding-40px-bottom sm-padding-30px-bottom">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <!-- start slogan -->
+                        <div class="col-lg-4 text-center text-md-start md-margin-three-bottom sm-margin-20px-bottom">
+                            <h6 class="text-dark-gray w-70 lg-w-100 no-margin-bottom">Branding & Media Agency</h6>
+                        </div>
+                        <!-- end slogan -->
+                        <!-- start contact information -->
+                        <div class="col-lg-4 col-md-6 text-center text-md-start sm-margin-20px-bottom">
+                            <span class="d-block">Lubumbashi, RD Congo.</span>
+                            <a href="mailto:sales@kreativetouch.agency" title="sales@kreativetouch.agency">sales@kreativetouch.agency</a>   |   +44 (0) 123 456 7890
+                        </div>
+                        <!-- end contact information -->
+                        <!-- start social media -->
+                        <div class="col-lg-4 col-md-6 social-style-2 text-center text-md-start">
+                            <a href="index.html"><img class="footer-logo" src="{{ asset('assets/images/logo-white.png') }}" data-at2x="{{ asset('assets/images/logo-white@2x.png') }}" alt="Pofo"></a>
+                            <div class="social-icon-style-8">
+                                <ul class="text-extra-small margin-20px-top sm-no-margin-bottom text-uppercase p-0 mb-0 list-unstyled">
+                                    <li class="d-inline-block margin-10px-right"><a href="http://twitter.com" target="_blank" title="Twitter">LinkedIn</a></li>
+                                    <li class="d-inline-block margin-10px-right"><a href="http://instagram.com" target="_blank" title="Instagram">Instagram</a></li>
+                                    <li class="d-inline-block"><a href="http://dribbble.com" target="_blank" title="Dribbble">Pinterst</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <!-- end social media -->
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <!-- start copyright -->
+                <div class="footer-bottom border-top border-color-medium-dark-gray padding-40px-top sm-padding-30px-top">
+                    <div class="row align-items-center">
+                        <div class="col-md-6 text-md-start text-center text-small">POFO - Portfolio Concept Theme</div>
+                        <div class="col-md-6 text-md-end text-center text-small">&copy; 2022 <a href="#" target="_blank" title="ThemeZaa">Kreative Touch</a></div>
+                    </div>
+                </div>
+                <!-- end copyright -->
+            </div>
+        </footer>
+        <!-- end footer -->
+        <!-- start scroll to top -->
+        <a class="scroll-top-arrow" href="javascript:void(0);"><i class="ti-arrow-up"></i></a>
+        <!-- end scroll to top  -->
+        <!-- javascript -->
+        <script type="text/javascript" src="{{ asset('assets/js/jquery.min.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('assets/js/bootsnav.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('assets/js/jquery.nav.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('assets/js/hamburger-menu.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('assets/js/theme-vendors.min.js') }}"></script>
+        <!-- setting -->
+        <script type="text/javascript" src="{{ asset('assets/js/main.js') }}"></script>
+    </body>
+</html>
