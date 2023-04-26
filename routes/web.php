e8e4e3899eb483fb7f9ca309ecf660d11a1b6514
emmamkb <emmanuelmakabu7@gmail.com>
2023-04-26 15:31:07 +0200

    ✨ Add services page

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
+
+Route::get('services/',function() {
+    return view('services.index');
+})->name('services');
